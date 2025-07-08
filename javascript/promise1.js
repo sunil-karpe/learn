f834eab575638775ca9f71b200c17ed1843566a8
sunil karpe <68121485+sunil-karpe@users.noreply.github.com>
2025-07-08 20:00:03 +0530

    added example for promise

--- javascript/promise1.js
+++ javascript/promise1.js
@@ -1,3 +1,34 @@
+/*
+const customPromise = new Promise((resolve,reject)=>{
+    let num=5; // any logic, it might be fetching data from server. 
+    if(num==5) {
+        resolve(num);
+    }else{
+        reject("number is not equal 5");
+    }
+})
+
+customPromise.then((num)=>{
+    console.log("value received::"+num);
+}).catch((err)=>{
+    console.log("error occured::"+err);
+}).finally(()=>{
+    console.log("end of operation");
+})
+
+*/
+
+/*example 1*/
+const jsonPromise = fetch('https://jsonplaceholder.typicode.com/todos');
+
+jsonPromise.then(response => response.json())  // parse JSON from response
+    .then(data => console.log(data))    // log the JSON data
+    .catch(error => console.error('Error:', error))
+    .finally(() => {
+        console.log("Operation performed");
+    })
+
+/*example 2*/
 let appInstalled=false;
 let login=false;
 
